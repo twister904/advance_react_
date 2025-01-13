aceb222fb2efc2517dfed5d9f4badc6407e0a2b2
Mazhar saifi <84468625+twister904@users.noreply.github.com>
2025-01-14 01:04:26 +0530

    useEffect learning with cleanup function and conditional renderding

--- src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,33 @@
+import { useEffect, useState } from "react";
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+  const [toggle, setToggle] = useState(false);
+  console.log("renders");
+  return (
+    <div>
+      <button
+        className='btn'
+        onClick={() => {
+          setToggle(!toggle);
+        }}
+      >
+        toggle component
+      </button>
+      {toggle && <RandomComponent />}
+    </div>
+  );
+};
+const RandomComponent = () => {
+  useEffect(() => {
+    console.log("hmm , this is useEffect console tag");
+    const intId = setInterval(() => {
+      console.log("hello from setInterval.....");
+    }, 1000);
+    return () => {
+      clearInterval(intId);
+      console.log("cleanup function called...");
+    };
+  }, []);
+  return <h1>Hello Champion</h1>;
 };
 
 export default CleanupFunction;
--- src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,5 +1,37 @@
+import { useState } from "react";
 const UserChallenge = () => {
-  return <h2>user challenge</h2>;
+  const [user, setUser] = useState(null);
+
+  const loggedOut = () => {
+    setUser(null);
+  };
+  const loggedIn = () => {
+    //  normally connect to db or api
+    setUser({ name: "Naruto Uzumaki the 9 tailed jinchuriki" });
+  };
+  const InKonoha = () => {
+    return (
+      <div style={{ backgroundColor: "orange" }}>
+        <h2>Hello {user.name}</h2>
+        <p>Welcome to Konoha The hidden leaf village</p>
+        <button onClick={loggedOut} className='btn'>
+          Press this button for the mission
+        </button>
+      </div>
+    );
+  };
+  const OnMission = () => {
+    return (
+      <div style={{ backgroundColor: "Yellow" }}>
+        <h2>Now You are on mission</h2>
+        <p>You will be Hokage one day</p>
+        <button onClick={loggedIn} className='btn'>
+          Press this button for get back to Konoha
+        </button>
+      </div>
+    );
+  };
+  return <div>{user ? <InKonoha /> : <OnMission />}</div>;
 };
 
 export default UserChallenge;
