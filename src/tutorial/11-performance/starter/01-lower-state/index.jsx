1be3559f88098a03c7acba161a174fface3a1c2e
Mazhar saifi <84468625+twister904@users.noreply.github.com>
2025-02-03 22:32:40 +0530

    learn how to improved the performance of ReactAPP by avoding re render

--- src/tutorial/11-performance/starter/01-lower-state/index.jsx
+++ src/tutorial/11-performance/starter/01-lower-state/index.jsx
@@ -1,13 +1,19 @@
 import { data } from "../../../../data";
 import { useState } from "react";
-import Counter from "./Counter";
+
 import List from "./List";
 const LowerState = () => {
   const [people, setPeople] = useState(data);
-
+  const [count, setCount] = useState(0);
   return (
     <section>
-      <Counter />
+      <button
+        className='btn'
+        onClick={() => setCount(count + 1)}
+        style={{ marginBottom: "1rem" }}
+      >
+        count {count}
+      </button>
       <List people={people} />
     </section>
   );
